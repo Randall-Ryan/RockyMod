846925667294b19cbbfc9b6dbd0b4114c1178743
Randall-Ryan <randyryan@Randys-PC>
2024-07-14 20:44:37 -0400

    Add custom fuel

--- src/main/java/net/rocky/rockymod/item/ModItems.java
+++ src/main/java/net/rocky/rockymod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.rocky.rockymod.RockyMod;
+import net.rocky.rockymod.item.custom.FuelItem;
 import net.rocky.rockymod.item.custom.MetalDetectorItem;
 
 public class ModItems {
@@ -16,6 +17,7 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties().durability(100)));
     public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry", () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone", () -> new FuelItem(new Item.Properties(), 400));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
